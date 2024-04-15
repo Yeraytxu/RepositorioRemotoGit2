8bc3c7470f1e4fe9daae95bfb46ac44ac600b2d0
yeray <yeray@Yeray>
2024-04-15 19:09:18 +0200

    Commit con mensajes nuevos

--- EjemploscontrolVersionesGit/src/MensajeDos.java
+++ EjemploscontrolVersionesGit/src/MensajeDos.java
@@ -4,6 +4,9 @@ public class MensajeDos {
 	public static void main(String[] args) {
 		System.out.println("Este es uno de dos mensajes en pantalla");
 		System.out.println("Este es el segundo de los dos mensajes en pantalla");
+		
+		System.out.println("Nuevo mensaje 1");
+		System.out.println("Nuevo mensaje 2");
 
 	}
 
