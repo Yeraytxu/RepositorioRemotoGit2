87bc9c2c18c1a191a05940d14a0704796c8d51ad
yeray <yeray@Yeray>
2024-04-15 18:59:15 +0200

    Commit

---
+++ EjemploscontrolVersionesGit/src/MensajeDos.java
@@ -0,0 +1,10 @@
+
+public class MensajeDos {
+
+	public static void main(String[] args) {
+		System.out.println("Este es uno de dos mensajes en pantalla");
+		System.out.println("Este es el segundo de los dos mensajes en pantalla");
+
+	}
+
+}
